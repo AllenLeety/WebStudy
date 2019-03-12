9ebee9103c12a8bec880f0e2eadba7493cab1c1d
AllenLeety <allenlee1228@yeah.net>
2019-03-12 21:47:01 +0800

    修改

--- systemStudy/look_fork.c
+++ systemStudy/look_fork.c
@@ -11,6 +11,7 @@
 #include <sys/stat.h>
 #include <string.h>
 
+int counter=200;
 int main(int argc, const char* argv[])
 {
 	int i=0;
@@ -31,22 +32,30 @@ int main(int argc, const char* argv[])
 	//first
 	if(i == 0)
 	{
+		counter +=200;
 		printf("first process, pid = %d\n", getpid());
+		printf("--counter = %d\n", counter);
 	}
 	//second
 	if(i == 1)
 	{
+		counter =+200;
 		printf("second process, pid = %d\n", getpid());
+		printf("--counter = %d\n", counter);
 	}
 	//third
  	if(i == 2)
 	{
+		counter +=200;
 		printf("third process, pid = %d\n", getpid());
+		printf("--counter = %d\n", counter);
 	}
 	//parent
 	if(i == 3)
 	{
+		counter +=400;
 		printf("parent process, pid = %d\n", getpid());
+		printf("--counter =%d\n", counter);
 	}
 
 	return 0;
