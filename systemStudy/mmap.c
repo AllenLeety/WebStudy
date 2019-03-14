d252c21aa521dba83160d33e6d49afdea2d44255
AllenLeety <allenlee1228@yeah.net>
2019-03-14 10:47:36 +0800

    mmap示例

---
+++ systemStudy/mmap.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+int main(int argc, const char* argv[])
+{
+  //打开一个文件
+  int fd = open("english.txt", O_RDWR);
+  int len = lseek(fd, 0, SEEK_END);
+
+  //创建内存映射区
+  void *ptr = mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if(ptr == MAP_FAILED)
+  {
+    perror("mmap error");
+    exit(1);
+  }
+
+  printf("%s", (char*)ptr);
+
+  //释放内存映射区
+  munmap(ptr, len);
+  close(fd);
+
+  return 0;
+}
